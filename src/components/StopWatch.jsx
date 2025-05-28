a323d3c4b9d1fc3579aa9364521cbefa3b517c67
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-05-29 00:37:08 +0900

    ✨ StopWatch 컴포넌트 추가
    공부 시간을 잴 수 있는 스탑와치 기능 추가 / 시작 버튼을 누르면 스탑와치 시작 / 일시 정지 버튼을 누르면 일시 정지 가능 / 이어서 버튼 누르면 이어서 시간 재기 가능 / 종료 버튼 누르면 스탑와치 초기화

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -1,13 +1,95 @@
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
   background-color: #ffffff;
   border-radius: 0.3rem;
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 0;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-size: 0.8rem;
+  font-weight: 500;
+`;
+
+const StopWatchTime = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+const ButtonsWrapper = styled.div`
+  width: 100%;
+  display flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const StopWatchButton = styled.button`
+  border: 0;
+  background-color: #af79fa;
+  color: #fff;
+  padding: 0.25rem 1.2rem;
+  border-radius: 0.8rem;
+  font-size: 0.75rem;
+  cursor: pointer;
 `;
 
 function StopWatch() {
-  return <Container>StopWatch</Container>;
+  const [isPaused, setPaused] = useState(false);
+  const [isRunning, setRunning] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(0);
+
+  const minutes = String(Math.floor(remainingTime / 60)).padStart(2, "0");
+  const seconds = String(remainingTime % 60).padStart(2, "0");
+
+  useEffect(() => {
+    if (isRunning && !isPaused) {
+      const relaxTimer = setInterval(() => {
+        setRemainingTime((prev) => {
+          return prev + 1;
+        });
+      }, 1000);
+      return () => clearInterval(relaxTimer);
+    }
+    if (!isRunning) {
+      setPaused(false);
+      setRemainingTime(0);
+    }
+  }, [isRunning, isPaused]);
+
+  return (
+    <Container>
+      <Title>StopWatch</Title>
+      <StopWatchTime>
+        {minutes} : {seconds}
+      </StopWatchTime>
+
+      {!isRunning && (
+        <StopWatchButton onClick={() => setRunning((prev) => !prev)}>
+          {/* {isClicked ? "일시 정지" : "시작"} */}
+          시작
+        </StopWatchButton>
+      )}
+      {isRunning && (
+        <ButtonsWrapper>
+          <StopWatchButton onClick={() => setPaused((prev) => !prev)}>
+            {isPaused ? "이어서" : "일시 정지"}
+          </StopWatchButton>
+          <StopWatchButton onClick={() => setRunning((prev) => !prev)}>
+            종료
+          </StopWatchButton>
+        </ButtonsWrapper>
+      )}
+    </Container>
+  );
 }
 
 export default StopWatch;
