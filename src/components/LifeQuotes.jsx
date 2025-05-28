5f71f5dc7d5cda673af79447ee7aa36a9c5e3a8e
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-05-28 18:16:32 +0900

    ✨ LifeQuotes 컴포넌트 추가
    최초 렌더링 또는 컴포넌트 클릭 시 저장된 명언 중 랜덤으로 하나 선택되어 화면에 나오도록 상태 및 함수 추가

--- src/components/LifeQuotes.jsx
+++ src/components/LifeQuotes.jsx
@@ -1,13 +1,63 @@
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
+  padding: 1rem 2.5rem;
   background-color: #ffffff;
   border-radius: 0.3rem;
-  flex: 1;
+  flex-basis: content;
+  font-size: 0.75rem;
+  transition: background-color 0.5s ease-in-out;
+  cursor: pointer;
+  user-select: none;
+
+  &:hover {
+    background-color: rgb(231, 220, 245);
+  }
 `;
 
+const Quote = styled.div`
+  line-height: 1.5;
+`;
+
+const Author = styled.div`
+  text-align: right;
+`;
+
+const quotes = [
+  {
+    id: 1,
+    author: "Steve Jobs",
+    quote: "The only way to do great work is to love what you do.",
+  },
+  {
+    id: 2,
+    author: "Carol Burnett",
+    quote: "When you have a dream, you've got to grab it and never let go.",
+  },
+  {
+    id: 3,
+    author: "Mohatma Gandhi",
+    quote: "The future depends on what we do in the present.",
+  },
+];
+
 function LifeQuotes() {
-  return <Container>LifeQuotes</Container>;
+  const [currentQuote, setCurrentQuote] = useState("");
+  const [isClicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    const randomNum = Math.floor(Math.random() * 3);
+    setCurrentQuote(quotes[randomNum]);
+    setClicked(false);
+  }, [isClicked]);
+
+  return (
+    <Container onClick={() => setClicked(true)}>
+      <Quote>{currentQuote.quote}</Quote>
+      <Author>- {currentQuote.author}</Author>
+    </Container>
+  );
 }
 
 export default LifeQuotes;
