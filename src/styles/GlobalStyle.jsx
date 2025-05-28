6bcf8730ba52a8a72cafe5b25a48fe94f777b5c0
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-05-28 15:21:50 +0900

    🚧 폴더 추가 및 스타일 초기화
    components, pages, styles 폴더 추가 / GlobalStyle 컴포넌트에 reset-css 적용 및 전역 스타일 지정 / Main.jsx에 GlobalStyle 적용 / App 컴포넌트에 필요 컴포넌트 임시 작성

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+import { Global, css } from "@emotion/react";
+import "reset-css";
+
+const GlobalStyle = () => {
+  return (
+    <Global
+      styles={css`
+        html,
+        body {
+          width: 100%;
+          height: 100%;
+          box-sizing: border-box;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: pink;
+        }
+      `}
+    />
+  );
+};
+
+export default GlobalStyle;
