965d80959034abf403f746271d36489e78fdaad4
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-05-28 16:48:32 +0900

    💄 Header 컴포넌트와 그 외 컴포넌트를 담은 div 간에 gap 적용

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ const AppContainer = styled.div`
   width: 30rem;
   height: 20rem;
   background-color: skyblue;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
 
 function App() {
   return (
     <AppContainer>
       <Header />
-      {/* <LifeQuotes />
-      <TodoList />
-      <Timer />
-      <StopWatch /> */}
+      <div style={{ backgroundColor: "purple", flex: "1" }}>
+        {/* <LifeQuotes />
+        <TodoList />
+        <Timer />
+        <StopWatch /> */}
+      </div>
     </AppContainer>
   );
 }
