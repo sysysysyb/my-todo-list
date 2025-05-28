dbb4775948a6e9c2e3f5fd6d513fbbe5af4e116a
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-05-29 00:12:25 +0900

    ✨ Timer 컴포넌트 추가
    쉬는 시간을 가질 때 사용할 10분 타이머 기능 추가 / 시작 버튼 클릭하면 타이머 시작 / 종료 버튼 누르면 타이머 초기화

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,13 +1,74 @@
 import styled from "@emotion/styled";
+import { useEffect, useRef, useState } from "react";
+import { addMinutes, format, subSeconds } from "date-fns";
 
 const Container = styled.div`
   background-color: #ffffff;
   border-radius: 0.3rem;
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 0;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-size: 0.8rem;
+  font-weight: 500;
+`;
+
+const TimerTime = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+const TimerButton = styled.button`
+  border: 0;
+  background-color: #af79fa;
+  color: #fff;
+  padding: 0.25rem 1.2rem;
+  border-radius: 0.8rem;
+  font-size: 0.75rem;
+  cursor: pointer;
 `;
 
 function Timer() {
-  return <Container>Timer</Container>;
+  const [isClicked, setClicked] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(60 * 10);
+
+  const minutes = String(Math.floor(remainingTime / 60)).padStart(2, "0");
+  const seconds = String(remainingTime % 60).padStart(2, "0");
+
+  useEffect(() => {
+    if (isClicked) {
+      const relaxTimer = setInterval(() => {
+        setRemainingTime((prev) => {
+          if (prev <= 1) {
+            clearInterval(relaxTimer);
+            return;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+      return () => clearInterval(relaxTimer);
+    } else {
+      setRemainingTime(60 * 10);
+    }
+  }, [isClicked]);
+
+  return (
+    <Container>
+      <Title>쉬는 시간 Timer</Title>
+      <TimerTime>
+        {minutes} : {seconds}
+      </TimerTime>
+      <TimerButton onClick={() => setClicked((prev) => !prev)}>
+        {isClicked ? "종료" : "시작"}
+      </TimerButton>
+    </Container>
+  );
 }
 
 export default Timer;
